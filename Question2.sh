5a2b6a88c74a3154e05f012c3f44da7d0b04630d
Arman <uarman29@gmail.com>
2019-06-19 19:21:48 -0400

    Double

---
+++ Question2.sh
@@ -0,0 +1,22 @@
+
+for textfile in `ls *.txt`
+do
+	mv $textfile new_$textfile
+done
+
+for pyfile in `ls *.py`
+do
+	length=`wc -l $pyfile`
+	if [ $length -ge 10 ]
+	then
+		mv $pyfile long_$pyfile
+	else
+		mv $pyfile short_$pyfile	
+	fi
+done
+
+for bakfile in `ls *.bak`
+do
+	rm $bakfile
+done
+
--- c.py
+++ c.py
@@ -1,9 +1,5 @@
-This file will onyl contain 9 lines to test the script
+1
 2
 3
 4
 5
-6
-7
-8
-9
